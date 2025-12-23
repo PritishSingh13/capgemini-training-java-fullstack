28bdc31b3ae154807ca4a82e760b03dc017acb11
pritishsg13@gmail.com <[pritishsg13@gmail.com]>
2025-12-23 11:56:57 +0530

    Solved Problem2(Accenture PYQ)

--- Day-04/problem01.java
+++ Day-04/problem01.java
@@ -1,13 +1,48 @@
+/*WAP to check the given character is
+uppercase vowel uppercase consonant
+Lowercase vowel
+Lowercase consonant
+If the given character is uppercase vowel, print numbers from the range 10 to 20 using do while
+If the given character is lowercase vowel, print numbers from the range -10 to -20 usng while
+if the given character is uppercase consonant, print numbers from the range 25 to 15 using for
+ If the given character is lowercase consonant, print numbers from the range -25 to -15 using for loop
+*/
 import java.util.*;
-public class problem01{
-    public stataic void main(String[]args){
+class Q1 {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         char ch = sc.next().charAt(0);
-        if(ch == 'A' || ch ='E' || ch = 'I' || ch == 'O' || ch == 'U'){
-            int a = 10;
+        int n = 0;
+        if(ch == 'A' | ch == 'E' | ch == 'I' || ch == 'O' || ch== 'U'){
+            n=10;
             do{
-                System.out.println()
+                System.out.println(n);
+                n++;
+
+            }
+            while(n <=20);
+        }
+        else if(ch == 'a' | ch == 'e' | ch == 'i' || ch == 'o' || ch== 'u'){
+            n= -10;
+            while(n >= -20){
+                System.out.println(n);
+                n--;
             }
         }
+        else if(ch >= 'A' && ch <= 'Z'){
+            for( n= 25;n>= 15;n--){
+                System.out.println(n);
+            }
+        }
+        else if(ch >= 'a' && ch <= 'z'){
+            for(n = -25 ;n <= -15;n++){
+                System.out.println(n);
+            }
+        }
+        else{
+            System.out.println("GG");
+        }
+
     }
+
 }
---
+++ Day-04/problem02.java
@@ -0,0 +1,33 @@
+//Given a number n, for each integer i in the range from 1 to n inclusive, print out one value
+
+//
+/*
+if i is a multiple of both 3 and 5, print FizzBuzz
+if i is a multiple of both 3(but not 5), print Fizz
+if i is a multiple of both 5 (but not 3), print Buzz
+if i is a multiple of both 3 and 5, print the value of i
+*/
+
+
+import java.util.*;
+public class problem02 {
+    public static void main(String[] args) {
+        int n = 20;  // example value
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
