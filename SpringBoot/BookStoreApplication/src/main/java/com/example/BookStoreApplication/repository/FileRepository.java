4a722735072ac39d75d0ee558c9c95b0904c6554
Pritish Singh <pritishsg13@gmail.com>
2026-03-06 12:49:32 +0530

    Delete SpringBoot/BookStoreApplication directory

--- SpringBoot/BookStoreApplication/src/main/java/com/example/BookStoreApplication/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BookStoreApplication.repository;
-
-import com.example.BookStoreApplication.entity.FileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository <FileEntity,Long> {
-}
--- SpringBoot/BookStoreApplication/src/main/java/com/example/BookStoreApplication/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BookStoreApplication.repository;
-
-import com.example.BookStoreApplication.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
